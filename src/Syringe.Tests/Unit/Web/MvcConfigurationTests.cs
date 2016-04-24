e31adb909271a6afb5fc52c12cbc71bbe7b0a62e
Chris S. <mrshrinkray@gmail.com>
2016-04-24 23:04:57 +0100

    MVC config tests template, generated using http://codepen.io/yetanotherchris/full/grjQYK/

--- src/Syringe.Tests/Unit/Web/MvcConfigurationTests.cs
+++ src/Syringe.Tests/Unit/Web/MvcConfigurationTests.cs
@@ -1,26 +1,88 @@
 ﻿using System;
 using NUnit.Framework;
+using Syringe.Web;
 
 namespace Syringe.Tests.Unit.Web
 {
-    [TestFixture]
-    public class MvcConfigurationTests
+	[TestFixture]
+	public class MvcConfigurationTests
 	{
+		private MvcConfiguration CreateMvcConfiguration()
+		{
+			return new MvcConfiguration();
+		}
+
 		[Test]
 		public void Load_should_deserialize_from_json_file()
 		{
 			// given
+			var actual = CreateMvcConfiguration();
 
 			// when
 
 			// then
+			Assert.That(actual, Is.Not.Null);
 		}
 
-		// exception if file doesn't exist
+		[Test]
+		public void Load_should_throw_exception_if_file_doesnt_exist()
+		{
+			// given
+			var actual = CreateMvcConfiguration();
 
-		// Doesnt load configuration twice
-		// defaults to localhost if none set
-		// signalr is built
-		// signalr lazy works
-    }
+			// when
+
+			// then
+			Assert.That(actual, Is.Not.Null);
+		}
+
+		[Test]
+		public void Load_should_not_load_configuration_twice()
+		{
+			// given
+			var actual = CreateMvcConfiguration();
+
+			// when
+
+			// then
+			Assert.That(actual, Is.Not.Null);
+		}
+
+		[Test]
+		public void should_default_serviceurl_to_localhost_if_none_set()
+		{
+			// given
+			var actual = CreateMvcConfiguration();
+
+			// when
+
+			// then
+			Assert.That(actual, Is.Not.Null);
+		}
+
+		[Test]
+		public void signalr_url_should_be_built()
+		{
+			// given
+			var actual = CreateMvcConfiguration();
+
+			// when
+
+			// then
+			Assert.That(actual, Is.Not.Null);
+		}
+
+		[Test]
+		public void signalr_lazy_property_should_be_built()
+		{
+			// given
+			var actual = CreateMvcConfiguration();
+
+			// when
+
+			// then
+			Assert.That(actual, Is.Not.Null);
+		}
+
+	}
 }
