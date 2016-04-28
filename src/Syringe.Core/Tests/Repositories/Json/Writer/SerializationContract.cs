c51a2c9ee396141a3d6ea7df39f17d99a817817e
Simon C <simon_colmer@hotmail.com>
2016-04-28 08:59:56 +0100

    An example of how to filter unwanted props

---
+++ src/Syringe.Core/Tests/Repositories/Json/Writer/SerializationContract.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Syringe.Core.Tests.Repositories.Json.Writer
+{
+    public class SerializationContract : DefaultContractResolver
+    {
+        private readonly string[] _propertiesToIgnore = 
+        {
+            "Filename"
+        };
+
+        protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
+        {
+            List<JsonProperty> defaultList = base.CreateProperties(type, memberSerialization).ToList();
+
+            List<JsonProperty> filtered = defaultList
+                            .Where(x => !_propertiesToIgnore.Any(p => p.Equals(x.PropertyName, StringComparison.InvariantCultureIgnoreCase)))
+                            .ToList();
+
+            return filtered;
+        }
+
+        public static JsonSerializerSettings GetSettings()
+        {
+            var settings = new JsonSerializerSettings
+            {
+                ContractResolver = new SerializationContract()
+            };
+
+            return settings;
+        }
+    }
+}
