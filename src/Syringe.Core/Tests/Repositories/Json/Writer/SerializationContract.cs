fe92e4e88e8717d699ae8b892c8ba09605843a7a
Simon C <simon_colmer@hotmail.com>
2016-04-29 08:46:36 +0100

    json save is now selectively storing properties

--- src/Syringe.Core/Tests/Repositories/Json/Writer/SerializationContract.cs
+++ src/Syringe.Core/Tests/Repositories/Json/Writer/SerializationContract.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
@@ -8,9 +9,21 @@ namespace Syringe.Core.Tests.Repositories.Json.Writer
 {
     public class SerializationContract : DefaultContractResolver
     {
-        private readonly string[] _propertiesToIgnore = 
+        private readonly string[] _propertiesToIgnore =
         {
-            "Filename"
+            "Filename",
+            "TransformedValue"
+        };
+
+        private readonly PropertyInfo[] _propsToIgnore =
+        {
+            typeof (Test).GetProperty("Filename"),
+            typeof (Test).GetProperty("Position"),
+            typeof (Test).GetProperty("AvailableVariables"),
+            typeof (TestFile).GetProperty("Filename"),
+            typeof (TestFile).GetProperty("Environment"),
+            typeof (Variable).GetProperty("Order"),
+            typeof (Variable).GetProperty("Roles"),
         };
 
         protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
@@ -18,10 +31,24 @@ namespace Syringe.Core.Tests.Repositories.Json.Writer
             List<JsonProperty> defaultList = base.CreateProperties(type, memberSerialization).ToList();
 
             List<JsonProperty> filtered = defaultList
-                            .Where(x => !_propertiesToIgnore.Any(p => p.Equals(x.PropertyName, StringComparison.InvariantCultureIgnoreCase)))
-                            .ToList();
+                                .Where(x => !_propsToIgnore.Any(p => GetFullName(p) == GetFullName(x)))
+                                .ToList();
+
+            //List<JsonProperty> filtered = defaultList
+            //                .Where(x => !_propertiesToIgnore.Any(p => p.Equals(x.PropertyName, StringComparison.InvariantCultureIgnoreCase)))
+            //                .ToList();
 
             return filtered;
+        }
+
+        private string GetFullName(PropertyInfo propertyInfo)
+        {
+            return $"{propertyInfo?.DeclaringType?.FullName}.{propertyInfo?.Name}";
+        }
+
+        private string GetFullName(JsonProperty jsonProperty)
+        {
+            return $"{jsonProperty?.DeclaringType?.FullName}.{jsonProperty?.PropertyName}";
         }
 
         public static JsonSerializerSettings GetSettings()
