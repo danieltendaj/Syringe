b5cd401cf0d62191e741bf7d3f2f01b1127f944a
Hemang Rajyaguru <hemdagem@gmail.com>
2016-05-02 11:25:02 +0100

    added htmlhelper for active class for menu links

---
+++ src/Syringe.Web/HtmlHelpers/NavLinks.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace Syringe.Web.HtmlHelpers
+{
+    public static class NavLinks
+    {
+        public static MvcHtmlString Active(this HtmlHelper helper, string action, string controller)
+        {
+            string activeClass = "";
+            var routeData = helper.ViewContext.RouteData;
+            if (routeData.Values["action"].ToString() == action && routeData.Values["controller"].ToString() == controller)
+            {
+                activeClass = "active";
+            }
+            return new MvcHtmlString(activeClass);
+        }
+    }
+}
