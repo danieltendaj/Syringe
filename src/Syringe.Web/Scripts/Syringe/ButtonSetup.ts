043e8b6997d0121f84b52a48b0535d7fbd7ac941
Simon C <simon_colmer@hotmail.com>
2016-05-13 20:18:01 +0100

    Added UI for copying test file

--- src/Syringe.Web/Scripts/Syringe/ButtonSetup.ts
+++ src/Syringe.Web/Scripts/Syringe/ButtonSetup.ts
@@ -9,6 +9,10 @@ $(document).ready(function () {
     let rowHandler = new RowHandler(rowsToAdd);
     rowHandler.setupButtons();
 
+    function getFileExtension(fileName: string) {
+        return fileName.split(".").pop().toLowerCase();
+    }
+
     $("body").on("click", "#removeRow", function (e) {
         e.preventDefault();
         var formGroup = $(this).closest(".form-group");
@@ -41,6 +45,39 @@ $(document).ready(function () {
             if (result) {
                 form.submit();
                 return true;
+            }
+        });
+
+        return false;
+    });
+
+    $(".copy-file-button").on("click", function (e) {
+        e.preventDefault();
+
+        var that = $(this);
+        var form = that.closest("form");
+        var sourceFileName = form.find("[name='sourceTestFile']").val();
+        bootbox.prompt({
+            title: "What would you like to call the new file?",
+            value: sourceFileName,
+            callback: function (result) {
+                var canConvert: boolean = false;
+                if (result) {
+                    if (result.toLowerCase() != sourceFileName.toLowerCase()) {
+                        if (getFileExtension(result) == getFileExtension(sourceFileName)) {
+                            canConvert = true;
+                        }
+                    }
+                }
+
+                if (!canConvert) {
+                    bootbox.alert("Invalid copy name, please try again.");
+                }
+                else {
+                    form.find("[name='targetTestFile']").val(result);
+                    form.submit();
+                    return true;
+                }
             }
         });
 
