19059b3fb23665223ab2ab32a50d590be0200019
Chris S <mrshrinkray@gmail.com>
2016-05-20 09:58:30 +0100

    Update chocolateyinstall.ps1

--- src/Chocolatey/Syringe/tools/chocolateyinstall.ps1
+++ src/Chocolatey/Syringe/tools/chocolateyinstall.ps1
@@ -70,7 +70,7 @@ if ($arguments["restoreConfigs"] -eq "true")
 
 # Add the user "SyringeUser" for the service
 # TODO don't do this if the user exists
-$toolsDir\add-syringeuser.ps1
+Invoke-Expression "$toolsDir\add-syringeuser.ps1"
 
 # Install and start the service
 Write-Host "Installing the Syringe service." -ForegroundColor Green
