9dd20dd4222bc1be048cdcc020b7fd46c0186c65
Hemang Rajyaguru <hemdagem@gmail.com>
2016-05-21 14:32:10 +0100

    fixed bug #150

--- src/Syringe.Web/Scripts/Syringe/RowHandler.ts
+++ src/Syringe.Web/Scripts/Syringe/RowHandler.ts
@@ -12,7 +12,7 @@
 
         $.get(test.URL, function (html) {
             var panelBody = test.$Button.parent().next();
-            var formGroup = panelBody.find(".form-group:last-child"), rowNumber = 0;
+            var formGroup = panelBody.find(".form-group").last(), rowNumber = 0;
 
             if (formGroup.length !== 0) {
                 var firstInputName = formGroup.find("input:first").attr("name");
