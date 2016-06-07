0f0a8f881df2bd82f2d699a01bf0a17af6323416
Simon C <simon_colmer@hotmail.com>
2016-06-07 08:33:27 +0100

    Added ability to boot either debug or release

--- tools/start-service.ps1
+++ tools/start-service.ps1
@@ -1,2 +1,17 @@
-$servicePath = Resolve-Path "$PSScriptRoot\..\src\Syringe.Service\bin\Debug\Syringe.Service.exe"
-& $servicePath 
+$debugPath = "$PSScriptRoot\..\src\Syringe.Service\bin\Debug\Syringe.Service.exe"
+$releasePath = "$PSScriptRoot\..\src\Syringe.Service\bin\Release\Syringe.Service.exe"
+
+if(Test-Path $debugPath)
+{
+		Write-Host "Running DEBUG" -foreground red
+		& $debugPath 
+}
+elseif(Test-Path $releasePath)
+{
+		Write-Host "Running RELEASE" -foreground red
+		& $releasePath 
+}
+else
+{
+		throw "Error, unable to find both debug and release service binaries"
+}
