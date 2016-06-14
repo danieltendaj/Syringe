5678ebcd76b216f031d38dbd21c3e86117370c82
Simon C <simon_colmer@hotmail.com>
2016-06-14 18:04:51 +0100

    Added RabdomNumberVariable and tests

--- src/Syringe.Core/Tests/Variables/ReservedVariables/RandomNumberVariable.cs
+++ src/Syringe.Core/Tests/Variables/ReservedVariables/RandomNumberVariable.cs
@@ -1,13 +1,17 @@
-﻿namespace Syringe.Core.Tests.Variables.ReservedVariables
+﻿using System;
+
+namespace Syringe.Core.Tests.Variables.ReservedVariables
 {
     public class RandomNumberVariable : IReservedVariable
     {
         public string Description => "Returns a random number each time it is used.";
         public string Name => "_randomNumber";
+        private readonly Random _random = new Random();
 
         public Variable CreateVariable()
         {
-            throw new System.NotImplementedException();
+            string randomNumber = _random.Next().ToString();
+            return new Variable(Name, randomNumber, string.Empty);
         }
     }
 }
