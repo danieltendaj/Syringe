d84eb0e3a3f348d35dbc1e43fb795e629c59615f
Chris S <mrshrinkray@gmail.com>
2016-06-16 15:22:28 +0100

    Update chocolateyinstall.ps1

--- src/Chocolatey/Syringe/tools/chocolateyinstall.ps1
+++ src/Chocolatey/Syringe/tools/chocolateyinstall.ps1
@@ -16,7 +16,7 @@ $syringeUsername = "SyringeUser"
 $syringePassword = "Passw0rd123"
 
 $version = "{{VERSION}}"
-$url = "https://yetanotherchris.blob.core.windows.net/syringe/Syringe-$version.zip"
+$url = "https://portalvhdshhv3xmk8gh157.blob.core.windows.net/syringe/Syringe-$version.zip"
 $url64 = $url
 
 . "$toolsDir\common.ps1"
