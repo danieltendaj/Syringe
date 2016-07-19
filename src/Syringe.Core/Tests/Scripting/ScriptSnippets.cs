8bfd7347c0e8ada495a6138aaa13aba61628594f
Chris S. <mrshrinkray@gmail.com>
2016-07-19 21:31:52 +0100

    Namespace cleanup and comments

--- src/Syringe.Core/Tests/Scripting/ScriptSnippets.cs
+++ src/Syringe.Core/Tests/Scripting/ScriptSnippets.cs
@@ -1,5 +1,9 @@
-﻿namespace Syringe.Core.Tests
+﻿namespace Syringe.Core.Tests.Scripting
 {
+	/// <summary>
+	/// Contains information about all snippet files for a test,
+	/// currently just snippets that execute before a test runs.
+	/// </summary>
     public class ScriptSnippets
     {
         public string BeforeExecuteFilename { get; set; }
