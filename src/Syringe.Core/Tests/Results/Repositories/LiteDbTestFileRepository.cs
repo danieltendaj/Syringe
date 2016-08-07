f5c6760ac8e651eca3b3b1bac3a80f915109b8f0
Chris S <mrshrinkray@gmail.com>
2016-08-07 17:00:38 +0100

    refactor: Made LiteDbTestFileRepository use a transaction and instance collection

--- src/Syringe.Core/Tests/Results/Repositories/LiteDbTestFileRepository.cs
+++ src/Syringe.Core/Tests/Results/Repositories/LiteDbTestFileRepository.cs
@@ -7,140 +7,121 @@ using LiteDB;
 
 namespace Syringe.Core.Tests.Results.Repositories
 {
-    public class LiteDbTestFileRepository : ITestFileResultRepository
-    {
-        private readonly object _lock = new object();
-        private readonly string _databaseLocation;
-        private LiteDatabase _database;
+	public class LiteDbTestFileRepository : ITestFileResultRepository
+	{
+		private readonly LiteDatabase _database;
+		private readonly string _databasePath;
+		private LiteTransaction _transaction;
+		private LiteCollection<TestFileResult> _collection;
 
-        public LiteDbTestFileRepository()
-        {
-            _databaseLocation = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "syringe.db");
+		public LiteDbTestFileRepository()
+		{
+			_databasePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "syringe.db");
+			_database = new LiteDatabase(_databasePath);
+			_collection = _database.GetCollection<TestFileResult>("results");
 
-            BsonMapper.Global.RegisterType<TimeSpan>
-            (
-                serialize: (timeSpan) => timeSpan.ToString(),
-                deserialize: (stringValue) => TimeSpan.Parse(stringValue)
-            );
-        }
+			// Use a transaction for the lifetime of the instance
+			_transaction = _database.BeginTrans();
 
-        public async Task AddAsync(TestFileResult testFileResult)
-        {
-            await Task.Run(() =>
-            {
-                lock (_lock)
-                {
-                    LiteCollection<TestFileResult> collection = GetCollection();
-                    collection.Insert(testFileResult);
-                }
-            });
-        }
+			// This can be removed in future versions of LiteDB when support for TimeSpan appears.
+			BsonMapper.Global.RegisterType<TimeSpan>
+			(
+				serialize: (timeSpan) => timeSpan.ToString(),
+				deserialize: (stringValue) => TimeSpan.Parse(stringValue)
+			);
+		}
 
-        public async Task DeleteAsync(Guid testFileResultId)
-        {
-            await Task.Run(() =>
-            {
-                lock (_lock)
-                {
-                    LiteCollection<TestFileResult> collection = GetCollection();
-                    collection.Delete(x => x.Id == testFileResultId);
-                }
-            });
-        }
+		public async Task AddAsync(TestFileResult testFileResult)
+		{
+			await Task.Run(() =>
+			{
+				_collection.Insert(testFileResult);
+			});
+		}
 
-        // TODO: Why isn't this async and all the other are?
-        public TestFileResult GetById(Guid id)
-        {
-            lock (_lock)
-            {
-                LiteCollection<TestFileResult> collection = GetCollection();
-                return collection.FindOne(x => x.Id == id);
-            }
-        }
+		public async Task DeleteAsync(Guid testFileResultId)
+		{
+			await Task.Run(() =>
+			{
+				_collection.Delete(x => x.Id == testFileResultId);
+			});
+		}
 
-        public async Task<TestFileResultSummaryCollection> GetSummaries(DateTime fromDate, int pageNumber = 1, int noOfResults = 20, string environment = "")
-        {
-            return await Task.Run(() =>
-            {
-                lock (_lock)
-                {
-                    LiteCollection<TestFileResult> collection = GetCollection();
+		public IEnumerable<TestFileResult> GetAll()
+		{
+			return _collection.FindAll();
+		}
 
-                    var startTimeQuery = Query.GTE("StartTime", fromDate);
-                    var query = string.IsNullOrEmpty(environment) 
-                                            ? startTimeQuery
-                                            : Query.And(startTimeQuery, Query.EQ("Environment", environment));
-                    
-                    long totalResults = collection
-                                            .Find(query)
-                                            .Count();
-            
-                    List<TestFileResult> testFileCollection = collection
-                        .Find(query)
-                        .OrderByDescending(x => x.StartTime)
-                        .Skip((pageNumber - 1) * noOfResults)
-                        .Take(noOfResults)
-                        .ToList();
+		// TODO: Why isn't this async and all the other are?
+		public TestFileResult GetById(Guid id)
+		{
+			return _collection.FindOne(x => x.Id == id);
+		}
 
-                    var result = new TestFileResultSummaryCollection
-                    {
-                        TotalFileResults = totalResults,
-                        PageNumber = pageNumber,
-                        NoOfResults = noOfResults,
-                        PageNumbers = Math.Ceiling((double)totalResults / noOfResults),
-                        PagedResults = testFileCollection
-                            .Select(x => new TestFileResultSummary()
-                            {
-                                Id = x.Id,
-                                DateRun = x.StartTime,
-                                FileName = x.Filename,
-                                TotalRunTime = x.TotalRunTime,
-                                TotalPassed = x.TotalTestsPassed,
-                                TotalFailed = x.TotalTestsFailed,
-                                TotalRun = x.TotalTestsRun,
-                                Environment = x.Environment,
-                                Username = x.Username
-                            })
-                    };
+		public async Task<TestFileResultSummaryCollection> GetSummaries(DateTime fromDate, int pageNumber = 1, int noOfResults = 20, string environment = "")
+		{
+			// Find/Query seems to need the transaction to be committed before running.
+			if (_transaction.State == TransactionState.Started)
+			{
+				_transaction.Commit();
+				_transaction = _database.BeginTrans();
+			}
 
-                    return result;
-                }
-            });
-        }
+			return await Task.Run(() =>
+			{
+				var startTimeQuery = Query.GTE("StartTime", fromDate);
+				var query = string.IsNullOrEmpty(environment)
+										? startTimeQuery
+										: Query.And(startTimeQuery, Query.EQ("Environment", environment));
 
-        private LiteCollection<TestFileResult> GetCollection()
-        {
-            if (_database == null)
-            {
-                _database = new LiteDatabase(_databaseLocation);
-            }
+				long totalResults = _collection
+										.Find(query)
+										.Count();
 
-            var collection = _database.GetCollection<TestFileResult>("results");
-            return collection;
-        }
+				List<TestFileResult> testFileCollection = _collection
+					.Find(query)
+					.OrderByDescending(x => x.StartTime)
+					.Skip((pageNumber - 1) * noOfResults)
+					.Take(noOfResults)
+					.ToList();
 
-        public void Wipe()
-        {
-            lock (_lock)
-            {
-                if (_database == null)
-                {
-                    _database = new LiteDatabase(_databaseLocation);
-                }
+				var result = new TestFileResultSummaryCollection
+				{
+					TotalFileResults = totalResults,
+					PageNumber = pageNumber,
+					NoOfResults = noOfResults,
+					PageNumbers = Math.Ceiling((double)totalResults / noOfResults),
+					PagedResults = testFileCollection
+						.Select(x => new TestFileResultSummary()
+						{
+							Id = x.Id,
+							DateRun = x.StartTime,
+							FileName = x.Filename,
+							TotalRunTime = x.TotalRunTime,
+							TotalPassed = x.TotalTestsPassed,
+							TotalFailed = x.TotalTestsFailed,
+							TotalRun = x.TotalTestsRun,
+							Environment = x.Environment,
+							Username = x.Username
+						})
+				};
 
-                _database?.DropCollection("results");
-            }
-        }
+				return result;
+			});
+		}
 
-        public void Dispose()
-        {
-            if (_database != null)
-            {
-                lock (_lock)
-                {
-                    _database.Dispose();
-                }
-            }
-        }
-    }
+		public void Wipe()
+		{
+			_database.DropCollection("results");
+		}
+
+		public void Dispose()
+		{
+			if (_transaction.State == TransactionState.Started)
+				_transaction.Commit();
+
+			_transaction?.Dispose();
+			_database?.Dispose();
+		}
+	}
 }
