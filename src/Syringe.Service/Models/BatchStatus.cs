dfa084babaa49e7ab878fd048aaccd0dca2185b2
Simon C <simon_colmer@hotmail.com>
2016-08-23 22:10:53 +0100

    Pushed TestFilesResult down to ease Json reading

--- src/Syringe.Service/Models/BatchStatus.cs
+++ src/Syringe.Service/Models/BatchStatus.cs
@@ -5,8 +5,8 @@ namespace Syringe.Service.Models
     public class BatchStatus
     {
         public int BatchId { get; set; }
-        public List<TestFileRunResult> TestFilesResult { get; set; }
         public bool Completed { get; set; }
         public bool AllTestsPassed { get; set; }
+        public List<TestFileRunResult> TestFilesResult { get; set; }
     }
 }
