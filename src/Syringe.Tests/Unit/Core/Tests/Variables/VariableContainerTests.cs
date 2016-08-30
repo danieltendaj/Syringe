7120ff65b9f056047d1f973cbaf8291751641577
SimonColmer <simon.colmer@totaljobsgroup.com>
2016-08-30 17:48:27 +0100

    Variables are now bundled together and returns grouped...
    ...this is so we can find more relevant variable sooner. We now only
    store variable against the current run as well

--- src/Syringe.Tests/Unit/Core/Tests/Variables/VariableContainerTests.cs
+++ src/Syringe.Tests/Unit/Core/Tests/Variables/VariableContainerTests.cs
@@ -1,57 +1,74 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Moq;
 using NUnit.Framework;
 using Syringe.Core.Tests.Variables;
 using Syringe.Core.Tests.Variables.ReservedVariables;
-using Syringe.Core.Tests.Variables.SharedVariables;
+using Syringe.Tests.StubsMocks;
 
 namespace Syringe.Tests.Unit.Core.Tests.Variables
 {
     [TestFixture]
     public class VariableContainerTests
     {
-        private Mock<IReservedVariableProvider> _reservedVariableProvider;
-        private Mock<ISharedVariablesProvider> _sharedVariableProvider;
-        private VariableContainer _variableContainer;
+        private ReservedVariableProviderStub _reservedVariableProvider;
+        private SharedVariablesProviderStub _sharedVariablesProvider;
 
         [SetUp]
         public void Setup()
         {
-            _reservedVariableProvider = new Mock<IReservedVariableProvider>();
-            _sharedVariableProvider = new Mock<ISharedVariablesProvider>();
-            _variableContainer = new VariableContainer(_reservedVariableProvider.Object, _sharedVariableProvider.Object);
+            _reservedVariableProvider = new ReservedVariableProviderStub();
+            _sharedVariablesProvider = new SharedVariablesProviderStub();
         }
 
         [Test]
         public void should_add_and_return_variable()
         {
             // given
-            var variable = new Variable();
+            const string environment = "YAY";
+            var variableContainer = new VariableContainer(environment, _reservedVariableProvider, _sharedVariablesProvider);
+            var variable = new Variable("my variable", "some value", environment);
 
             // when
-            _variableContainer.Add(variable);
+            variableContainer.Add(variable);
 
             // then
-            var result = _variableContainer.First(x => x == variable);
+            var result = variableContainer.First(x => x == variable);
             Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        public void should_not_add_or_return_variable_assigned_against_a_different_environment()
+        {
+            // given
+            const string environment = "YAY";
+            var variableContainer = new VariableContainer(environment, _reservedVariableProvider, _sharedVariablesProvider);
+            var variable = new Variable("my variable", "some value", "NOT ME");
+
+            // when
+            variableContainer.Add(variable);
+
+            // then
+            var result = variableContainer.FirstOrDefault(x => x == variable);
+            Assert.That(result, Is.Null);
         }
 
         [Test]
         public void should_return_reserved_variable()
         {
             // given
-            var expectedVariable = new Variable("namey", "valuey", "environy");
+            const string environment = "another-environment";
+            var expectedVariable = new Variable("namey", "valuey", environment);
             var reservedVariable = new Mock<IReservedVariable>();
             reservedVariable
                 .Setup(x => x.CreateVariable())
                 .Returns(expectedVariable);
 
-            _reservedVariableProvider
-                .Setup(x => x.ListAvailableVariables())
-                .Returns(new[] { reservedVariable.Object });
+            _reservedVariableProvider.ListAvailableVariables_Value = new[] { reservedVariable.Object };
 
             // when
-            var result = _variableContainer.FirstOrDefault(x => x == expectedVariable);
+            var variableContainer = new VariableContainer(environment, _reservedVariableProvider, _sharedVariablesProvider);
+            var result = variableContainer.FirstOrDefault(x => x == expectedVariable);
 
             // then
             Assert.That(result, Is.Not.Null);
@@ -61,16 +78,44 @@ namespace Syringe.Tests.Unit.Core.Tests.Variables
         public void should_return_system_variable()
         {
             // given
-            var expectedVariable = new Variable("some-other-name", "jingle-jangle", "innit");
-            _sharedVariableProvider
-                .Setup(x => x.ListSharedVariables())
-                .Returns(new[] { expectedVariable });
+            const string environment = "another-environment";
+            var expectedVariable = new Variable("some-other-name", "jingle-jangle", environment);
+            _sharedVariablesProvider.ListSharedVariables_Value = new IVariable[] { expectedVariable };
 
             // when
-            var result = _variableContainer.FirstOrDefault(x => x == expectedVariable);
+            var variableContainer = new VariableContainer(environment, _reservedVariableProvider, _sharedVariablesProvider);
+            var result = variableContainer.FirstOrDefault(x => x == expectedVariable);
 
             // then
             Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        public void should_prioritise_custom_variable_over_other_variables()
+        {
+            // given
+            const string name = "variables-are-super-awesome";
+            const string environment = "my-env";
+            _sharedVariablesProvider.ListSharedVariables_Value = new IVariable[]
+            {
+                new Variable("another-thing", "NOT ME", "something else"),
+                new Variable(name, "NOT ME", "something else"),
+                new Variable(name, "NOT ME", "another one"),
+                new Variable(name, "NOT ME", environment),
+                new Variable(name, string.Empty, environment),
+                new Variable("HELLO", "NOT ME", "CARS"),
+            };
+
+            var variableContainer = new VariableContainer(environment, _reservedVariableProvider, _sharedVariablesProvider);
+            var expectedVariable = new Variable(name, "expected value", environment);
+            variableContainer.Add(expectedVariable);
+
+            // when
+            List<IVariable> variables = variableContainer.ToList();
+            IVariable result = variables.First(x => x.Name == name && x.Environment.Name == environment);
+
+            // then
+            Assert.That(result, Is.EqualTo(expectedVariable));
         }
     }
 }
