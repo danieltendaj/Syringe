0ce5e116ed14feca42f63cc1b138591b653951a3
Simon C <simon_colmer@hotmail.com>
2016-09-04 15:02:18 +0100

    Added coverage around FilterConfig

--- src/Syringe.Web/App_Start/FilterConfig.cs
+++ src/Syringe.Web/App_Start/FilterConfig.cs
@@ -2,7 +2,7 @@
 
 namespace Syringe.Web
 {
-    public class FilterConfig
+    public static class FilterConfig
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
