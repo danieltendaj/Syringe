81a977a313d960494da1909cd9c24cd0d69d52b3
Simon C <simon_colmer@hotmail.com>
2016-09-05 07:59:12 +0100

    Getting DbCleanupJob stubbed out

---
+++ src/Syringe.Service/Jobs/DbCleanupJob.cs
@@ -0,0 +1,23 @@
+﻿using Syringe.Core.Tests.Results.Repositories;
+
+namespace Syringe.Service.Jobs
+{
+    public class DbCleanupJob : IDbCleanupJob
+    {
+        private readonly ITestFileResultRepository _repository;
+        public DbCleanupJob(ITestFileResultRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void Start()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
