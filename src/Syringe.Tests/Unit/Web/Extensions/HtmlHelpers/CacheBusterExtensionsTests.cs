4c2a611020b76fdd065d4f4a9001499c321003b3
Simon C <simon_colmer@hotmail.com>
2016-09-12 16:58:11 +0100

    Added code to generate cache buster

---
+++ src/Syringe.Tests/Unit/Web/Extensions/HtmlHelpers/CacheBusterExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using NUnit.Framework;
+using Syringe.Web.Extensions.HtmlHelpers;
+
+namespace Syringe.Tests.Unit.Web.Extensions.HtmlHelpers
+{
+    [TestFixture]
+    public class CacheBusterExtensionsTests
+    {
+        [Test]
+        public void should_return_expected_version_number()
+        {
+            // given
+
+            // when
+            HtmlString result = CacheBusterExtensions.GetCacheBuster(null);
+
+            // then
+            string expectedVersion = $"?v={CacheBusterExtensions.GetAssemblyVersion()}";
+            Assert.That(result.ToString(), Is.EqualTo(expectedVersion));
+        }
+    }
+}
---
+++ src/Syringe.Web/Extensions/HtmlHelpers/CacheBusterExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Syringe.Web.Extensions.HtmlHelpers
+{
+    public static class CacheBusterExtensions
+    {
+        public static HtmlString GetCacheBuster(this HtmlHelper htmlHelper)
+        {
+            return new HtmlString($"?v={GetAssemblyVersion()}");
+        }
+
+        public static string GetAssemblyVersion()
+        {
+            return Assembly.GetExecutingAssembly().GetName().Version.ToString().Replace(".", "-");
+        }
+    }
+}
