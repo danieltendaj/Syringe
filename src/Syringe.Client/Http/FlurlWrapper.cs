0e80460641c250b075de1b6fa78ac757dec04ea4
Chris S <mrshrinkray@gmail.com>
2016-09-16 15:01:12 +0100

    refactor: Added Client classes, changed to use the Flurl library

---
+++ src/Syringe.Client/Http/FlurlWrapper.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+using Flurl;
+using Flurl.Http;
+
+namespace Syringe.Client.Http
+{
+    public class FlurlWrapper
+    {
+        private FlurlClient _client;
+        public Flurl.Url Url { get; set; }
+
+        public FlurlWrapper(CustomHttpClientFactory httpClientFactory, string controllerPath)
+        {
+            _client = Url.ConfigureClient(x =>
+            {
+                x.CookiesEnabled = true;
+                x.HttpClientFactory = httpClientFactory;
+            });
+
+            Url = new Url(httpClientFactory.BaseUrl);
+            Url = Url.AppendPathSegment(controllerPath);
+        }
+
+        public async Task<T> Get<T>(string action)
+        {
+            return await Url.GetJsonAsync<T>();
+        }
+
+        public async Task<T> Post<T>(string action, object body)
+        {
+            return await Url.PostJsonAsync(body).ReceiveJson<T>();
+        }
+
+        public async Task<T> Patch<T>(string action, object body)
+        {
+            return await Url.PatchJsonAsync(body).ReceiveJson<T>();
+        }
+
+        public async Task<T> Delete<T>(string action)
+        {
+            return await Url.DeleteAsync().ReceiveJson<T>();
+        }
+
+        public void AddParameter(string name, string value)
+        {
+            Url = Url.SetQueryParam(name, value);
+        }
+    }
+}
---
+++ src/Syringe.Client/TestsClient.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading.Tasks;
+using Syringe.Client.Http;
+using Syringe.Core.Services;
+using Syringe.Core.Tests;
+using Syringe.Core.Tests.Results;
+
+namespace Syringe.Client
+{
+    public class TestsClient : ITestService
+    {
+        internal readonly string ServiceUrl;
+        private readonly FlurlWrapper _testFileWrapper;
+        private readonly FlurlWrapper _testWrapper;
+
+        public TestsClient(string serviceUrl)
+        {
+            ServiceUrl = serviceUrl;
+            var factory = new CustomHttpClientFactory(serviceUrl);
+
+            _testFileWrapper = new FlurlWrapper(factory, "/api/testfile");
+            _testWrapper = new FlurlWrapper(factory, "/api/test");
+        }
+
+        public IEnumerable<string> ListFiles()
+        {
+            return _testFileWrapper.Get<List<string>>("testfiles").Result;
+        }
+        
+        public TestFile GetTestFile(string filename)
+        {
+            _testFileWrapper.AddParameter("filename", filename);
+            return _testFileWrapper.Get<TestFile>("").Result;
+        }
+
+        public string GetRawFile(string filename)
+        {
+            _testFileWrapper.AddParameter("filename", filename);
+            return _testFileWrapper.Get<string>("raw").Result;
+        }
+
+        public bool EditTest(string filename, int position, Test test)
+        {
+            _testFileWrapper.AddParameter("filename", filename);
+            _testFileWrapper.AddParameter("position", Convert.ToString(position));
+            return _testFileWrapper.Patch<bool>("", test).Result;
+        }
+
+        public bool CreateTest(string filename, Test test)
+        {
+            _testWrapper.AddParameter("filename", filename);
+            return _testWrapper.Post<bool>("", test).Result;
+        }
+
+        public bool DeleteTest(int position, string filename)
+        {
+            _testWrapper.AddParameter("filename", filename);
+            _testWrapper.AddParameter("position", Convert.ToString(position));
+            return _testWrapper.Delete<bool>("").Result;
+        }
+
+        public bool CopyTest(int position, string filename)
+        {
+            _testWrapper.AddParameter("filename", filename);
+            _testWrapper.AddParameter("position", Convert.ToString(position));
+            return _testWrapper.Post<bool>("copy", "").Result;
+        }
+
+        public bool CreateTestFile(TestFile testFile)
+        {
+            _testFileWrapper.AddParameter("filename", testFile.Filename);
+            return _testFileWrapper.Post<bool>("copy", testFile).Result;
+        }
+
+        public bool CopyTestFile(string sourceFileName, string targetFileName)
+        {
+            _testFileWrapper.AddParameter("sourceFileName", sourceFileName);
+            _testFileWrapper.AddParameter("targetFileName", targetFileName);
+
+            return _testFileWrapper.Post<bool>("copy", "").Result;
+        }
+
+        public bool UpdateTestVariables(TestFile testFile)
+        {
+            return _testFileWrapper.Post<bool>("variables", testFile).Result;
+        }
+
+        public async Task<TestFileResultSummaryCollection> GetSummaries(DateTime fromDateTime, int pageNumber = 1, int noOfResults = 20, string environment = "")
+        {
+            _testWrapper.AddParameter("filename", pageNumber.ToString());
+            _testWrapper.AddParameter("noOfResults", noOfResults.ToString());
+            _testWrapper.AddParameter("fromDateTime", fromDateTime.ToString(CultureInfo.InvariantCulture));
+            _testWrapper.AddParameter("environment", environment);
+
+            return await _testWrapper.Get<TestFileResultSummaryCollection>("results");
+        }
+
+        public TestFileResult GetResultById(Guid id)
+        {
+            _testWrapper.AddParameter("id", id.ToString());
+            return _testWrapper.Get<TestFileResult>("result").Result;
+        }
+
+        public bool DeleteResult(Guid id)
+        {
+			_testWrapper.AddParameter("id", id.ToString());
+			return _testWrapper.Delete<bool>("result").Result;
+        }
+
+        public bool DeleteFile(string fileName)
+        {
+			_testFileWrapper.AddParameter("filename", fileName);
+			return _testFileWrapper.Delete<bool>("result").Result;
+        }
+    }
+}
