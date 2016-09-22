b666494f8d40d3466c8479129475f516b1086bde
Simon C <simon_colmer@hotmail.com>
2016-09-22 19:43:57 +0100

    Shared variiables are now getting loaded from config

---
+++ src/Syringe.Core/Tests/Variables/SharedVariables/SharedVariable.cs
@@ -0,0 +1,9 @@
+﻿namespace Syringe.Core.Tests.Variables.SharedVariables
+{
+    public class SharedVariable
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public string Environment { get; set; }
+    }
+}
--- src/Syringe.Core/Tests/Variables/SharedVariables/SharedVariablesProvider.cs
+++ src/Syringe.Core/Tests/Variables/SharedVariables/SharedVariablesProvider.cs
@@ -1,56 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Extensions.Configuration;
-using Newtonsoft.Json;
-using Syringe.Core.Configuration;
+using Microsoft.Extensions.Options;
 
 namespace Syringe.Core.Tests.Variables.SharedVariables
 {
     public class SharedVariablesProvider : ISharedVariablesProvider
     {
-        private readonly IConfigurationRoot _configurationRoot;
-        //private readonly IConfigLocator _configLocator;
+        private readonly IOptions<SharedVariables> _options;
         private Variable[] _sharedVariables;
 
-        //public SharedVariablesProvider(IConfigLocator configLocator)
-        //{
-        //    _configLocator = configLocator;
-        //}
-
-        public SharedVariablesProvider(IConfigurationRoot configurationRoot)
+        public SharedVariablesProvider(ConfigurationRoot configurationRoot, IOptions<SharedVariables> options)
         {
-            _configurationRoot = configurationRoot;
+            _options = options;
         }
 
         public IEnumerable<IVariable> ListSharedVariables()
         {
             if (_sharedVariables == null)
             {
-                try
+                if (_options.Value != null && _options.Value.Variables.Any())
                 {
-                    //string configPath = _configLocator.ResolveConfigFile("shared-variables.json");
-                    //string json = File.ReadAllText(configPath);
-                    //List<SharedVariable> variables = JsonConvert.DeserializeObject<List<SharedVariable>>(json);
-
-                    var variables = _configurationRoot.GetValue<List<SharedVariable>>("sharedVariables");
-                    _sharedVariables = variables.Select(x => new Variable(x.Name, x.Value, x.Environment)).ToArray();
+                    _sharedVariables = _options.Value.Variables.Select(x => new Variable(x.Name, x.Value, x.Environment)).ToArray();
                 }
-                catch (FileNotFoundException)
+                else
                 {
                     _sharedVariables = new Variable[0];
                 }
             }
 
             return _sharedVariables;
-        }
-
-        private class SharedVariable
-        {
-            public string Name { get; set; }
-            public string Value { get; set; }
-            public string Environment { get; set; }
         }
     }
 }
