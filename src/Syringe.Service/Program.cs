cec4d8b175eadeafbb5d366ab57de501eb181591
Chris S <chrisasmall+github@gmail.com>
2017-07-21 18:00:37 +0100

    Fix the nuget dependencies, set the service to port 1981

--- src/Syringe.Service/Program.cs
+++ src/Syringe.Service/Program.cs
@@ -3,18 +3,18 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace Syringe.Service
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var host = new WebHostBuilder()
+				.UseKestrel()
+				.UseUrls("http://0.0.0.0:1981")
+				.UseContentRoot(Directory.GetCurrentDirectory())
+				.UseStartup<Startup>()
+				.Build();
 
-            host.Run();
-        }
-    }
+			host.Run();
+		}
+	}
 }
--- src/Syringe.Web/Program.cs
+++ src/Syringe.Web/Program.cs
@@ -7,18 +7,17 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace Syringe.Web
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var host = new WebHostBuilder()
+				.UseKestrel()
+				.UseContentRoot(Directory.GetCurrentDirectory())
+				.UseStartup<Startup>()
+				.Build();
 
-            host.Run();
-        }
-    }
+			host.Run();
+		}
+	}
 }
