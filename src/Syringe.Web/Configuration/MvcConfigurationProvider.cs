4ee0ae789fa07140e1bc1dd74ee72b0b417ec5ac
Chris S <chrisasmall+github@gmail.com>
2017-07-22 15:46:16 +0100

    MVC: added configuration folder

---
+++ src/Syringe.Web/Configuration/MvcConfigurationProvider.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using Syringe.Core.Configuration;
+
+namespace Syringe.Web.Configuration
+{
+    public class MvcConfigurationProvider : IMvcConfigurationProvider
+    {
+        private readonly IConfigLocator _configLocator;
+        private MvcConfiguration _configuration;
+
+        public MvcConfigurationProvider(IConfigLocator configLocator)
+        {
+            _configLocator = configLocator;
+        }
+
+        public MvcConfiguration Load()
+        {
+            if (_configuration == null)
+            {
+                string configPath = _configLocator.ResolveConfigFile("websiteconfig.json");
+                string json = File.ReadAllText(configPath);
+                _configuration = JsonConvert.DeserializeObject<MvcConfiguration>(json);
+            }
+
+            return _configuration;
+        }
+    }
+}
